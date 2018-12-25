b1a48e730492077270a4726d0aea1a26cd91ca41
AymanBAHHOU <44382293+AymanBAHHOU@users.noreply.github.com>
2018-12-25 08:57:01 +0000

    Create GestionDesRhApplicationTests.java

---
+++ src/test/java/ma/ac/emi/info/GestionDesRhApplicationTests.java
@@ -0,0 +1,17 @@
+package org.emi.GestionDesRH;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestionDesRhApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
